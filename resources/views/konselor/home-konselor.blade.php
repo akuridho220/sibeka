aea872f5b5941d7114a54784dbad79012913ca96
akuridho220 <akuridho220@gmail.com>
2023-11-07 15:18:12 +0700

    update fe

--- resources/views/konselor/home-konselor.blade.php
+++ resources/views/konselor/home-konselor.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.main')
 @section('content')
     <div class="w-full overflow-x-hidden flex flex-col">
-        <main class="w-full flex-grow px-12 py-8 min-h-screen">
-            <p class="text-3xl font-bold primary-color pb-2">Jadwal Pertemuan</p>
-                <div class="box-content max-w-md border-1 p-6 rounded-lg mb-4" style="background-color: #0997BC;">
+        <main class="w-full flex-grow px-4 md:px-12 py-8 min-h-screen">
+            <p class="text-xl sm:text-2xl md:text-3xl  font-bold primary-color pb-2 ">Jadwal Pertemuan</p>
+                <div class="text-sm md:text-base box-content max-w-md border-1 p-4 md:p-6 rounded-lg mb-4" style="background-color: #0997BC;">
                     <div class="grid grid-cols-2">
                         <p class="font-semibold text-white">Hari / Tanggal</p>
                         <p class="font-semibold text-white">Senin, 2 Oktober 2023</p>
@@ -21,49 +21,54 @@
                         <p class="font-semibold text-white">Mahasiswa A</p>
                     </div> 
                 </div>
-                <button class="rounded-lg p-3" style="background-color: #159600; display: flex; justify-content: center; align-items: center;">
-                    <a href="/konselor-laporan" class="font-semibold text-white">Isi Laporan Konseling</a>
+                <button class="rounded-lg p-2 md:p-3" style="background-color: #159600; display: flex; justify-content: center; align-items: center;">
+                    <a href="/konselor-laporan" class="text-sm md:text-base font-semibold text-white">Isi Laporan Konseling</a>
                 </button>
-            <p class="text-3xl font-bold danger pt-6 pb-2">Laporan yang Belum Terisi</p>
-            
-            <div class="flex flex-col">
-                <div class="flex flex-row">
-                    <div class="box-content w-40 border-1 p-4 rounded-lg" style="background-color: #F6F1F1; display: flex; justify-content: center; align-items: center;">
+
+            <p class="text-xl md:text-3xl font-bold danger pt-6 pb-2">Laporan yang Belum Terisi</p>
+            <div class="text-sm md:text-base flex flex-col gap-5">
+                <div class="flex flex-row gap-5">
+                    <div class="box-content border-1 w-32 md:w-40 p-3 md:p-4 rounded-lg" style="background-color: #F6F1F1; display: flex; justify-content: center; align-items: center;">
                         <p class="font-semibold text-black">25 September 2023</p>
                     </div>
-                    <div class="box-content w-48 border-1 p-4 rounded-lg" style="background-color: #E96C6C; ">
-                        <div class="grid grid-rows-3">
-                            <p class="font-semibold text-black">09.00-11.00</p>
-                            <p class="font-semibold text-black">Ruang C</p>
-                            <p class="font-semibold text-black">Mahasiswa B</p>
+                    <div class="flex flex-col sm:flex-row gap-5 ">
+                        <div class="box-content w-36 md:w-48 border-1 p-3 md:p-4 rounded-lg" style="background-color: #E96C6C; ">
+                            <div class="grid grid-rows-3">
+                                <p class="font-semibold text-black">09.00-11.00</p>
+                                <p class="font-semibold text-black">Ruang C</p>
+                                <p class="font-semibold text-black">Mahasiswa B</p>
+                            </div>
                         </div>
-                    </div>
-                    <div class="box-content w-48 border-1 p-4 rounded-lg" style="background-color: #159600; display: flex; justify-content: center; align-items: center;">
-                        <button class="font-semibold text-black">Isi Laporan Konseling</button>
+                        <div class="box-content w-36 md:w-48 border-1 p-3 md:p-4 rounded-lg" style="background-color: #159600; display: flex; justify-content: center; align-items: center;">
+                            <button class="font-semibold text-white">Isi Laporan Konseling</button>
+                        </div>
                     </div>
                 </div>
-                <div class="flex flex-row">
-                    <div class="box-content w-40 border-1 p-4 rounded-lg" style="background-color: #F6F1F1; display: flex; justify-content: center; align-items: center;">
+                <div class="flex flex-row gap-5">
+                    <div class="box-content w-32 md:w-40 border-1 p-3 md:p-4 rounded-lg" style="background-color: #F6F1F1; display: flex; justify-content: center; align-items: center;">
                         <p class="font-semibold text-black">18 September 2023</p>
                     </div>
-                    <div class="box-content w-48 border-1 p-4 rounded-lg" style="background-color: #E96C6C;">
-                        <div class="grid grid-rows-3">
-                            <p class="font-semibold text-black">13.00-15.00</p>
-                            <p class="font-semibold text-black">Ruang A</p>
-                            <p class="font-semibold text-black">Mahasiswa C</p>
+                    <div class="flex flex-col sm:flex-row gap-5">
+                        <div class="box-content w-36 md:w-48 border-1 p-3 md:p-4 rounded-lg" style="background-color: #E96C6C;">
+                            <div class="grid grid-rows-3">
+                                <p class="font-semibold text-black">13.00-15.00</p>
+                                <p class="font-semibold text-black">Ruang A</p>
+                                <p class="font-semibold text-black">Mahasiswa C</p>
+                            </div>
                         </div>
-                    </div>
-                    <div class="box-content w-48 border-1 p-4 rounded-lg" style="background-color: #159600; display: flex; justify-content: center; align-items: center;">
-                        <button class="font-semibold text-black">Isi Laporan Konseling</button>
+                        <div class="box-content w-36 md:w-48 border-1 p-3 md:p-4 rounded-lg" style="background-color: #159600; display: flex; justify-content: center; align-items: center;">
+                            <button class="font-semibold text-white">Isi Laporan Konseling</button>
+                        </div>
                     </div>
                 </div>
             </div>
-            <p class="text-3xl font-bold primary-color pt-6 pb-2">Jadwal Pertemuan yang akan Datang</p>
-                <div class="grid grid-cols-2 gap-1 pb-6">
-                    <div class="box-content w-40 border-1 p-4 rounded-lg" style="background-color: #F6F1F1; display: flex; justify-content: center; align-items: center;">
-                        <p class="font-semibold text-[#515151]">25 September 2023</p>
+
+            <p class="text-xl md:text-3xl font-bold primary-color pt-6 pb-2">Jadwal Pertemuan yang akan Datang</p>
+                <div class="text-sm md:text-base flex gap-5 pb-6">
+                    <div class="box-content w-32 md:w-40 border-1 p-3 md:p-4 rounded-lg" style="background-color: #F6F1F1; display: flex; justify-content: center; align-items: center;">
+                        <p class="font-semibold text-[#515151]">6 Oktober 2023</p>
                     </div>
-                    <div class="box-content w-40 border-1 p-4 rounded-lg" style="background-color: #F6F1F1;">
+                    <div class="box-content w-32 md:w-40 border-1 p-3 md:p-4 rounded-lg" style="background-color: #F6F1F1;">
                         <div class="grid grid-rows-3">
                             <p class="font-semibold text-[#515151]">09.00-11.00</p>
                             <p class=" text-[#636363]">Ruang C</p>
--- resources/views/tim-konseling/persetujuan.blade.php
+++ resources/views/tim-konseling/persetujuan.blade.php
@@ -1,16 +1,14 @@
 @extends('layouts.main')
 @section('content')
-    <div class="w-full overflow-x-hidden flex flex-col">
-        <main class="w-full flex-grow p-6 min-h-screen">
-            <div class="container mx-auto">
-                <h2 class="text-2xl font-bold mb-4 text-center">Daftar Pengajuan Konseling</h2>
-
-                <table class="min-w-full bg-white border border-gray-300 text-center mx-10">
+    <div class="w-full overflow-x-auto flex flex-col">
+        <main class="w-full flex-grow md:p-6 min-h-screen">
+            <div class="container mx-auto lg:w-2/3 w-5/6" id="container">
+                <h2 class="text-3xl lg:text-4xl xl:text-5xl font-bold mb-4 text-center">Daftar Pengajuan Konseling</h2>
+                <table class="min-w-full bg-white border border-gray-300 text-center mx-auto">
                     <thead style="background-color: #0997BC;" class="text-white font-bold">
                         <tr>
                             <th class="py-2 px-4 border-b">No</th>
                             <th class="py-2 px-4 border-b">Pihak yang Mengajukan</th>
-                            <th class="py-2 px-4 border-b">Waktu</th>
                             <th class="py-2 px-4 border-b">Detail</th>
                         </tr>
                     </thead>
@@ -19,7 +17,6 @@
                         <tr>
                             <td class="py-2 px-4 border-b">1</td>
                             <td class="py-2 px-4 border-b">Konselor A</td>
-                            <td class="py-2 px-4 border-b">Waktu A</td>
                             <td class="py-2 px-4 border-b">
                                 <button id="openModal1" class="text-white py-1 px-2 rounded" style="background-color: #FFC436;">Detail</button>
                             </td>
@@ -28,7 +25,6 @@
                         <tr>
                             <td class="py-2 px-4 border-b">2</td>
                             <td class="py-2 px-4 border-b">Mahasiswa B</td>
-                            <td class="py-2 px-4 border-b">Waktu B</td>
                             <td class="py-2 px-4 border-b">
                                 <button id="openModal2" class="text-white py-1 px-2 rounded" style="background-color: #FFC436;">Detail</button>
                             </td>
@@ -36,116 +32,120 @@
                         <!-- Add more rows as needed -->
                     </tbody>
                 </table>
-            </div>           
+            </div>
+            <!-- Modal Konselor -->
+            <div id="myModal1" class="modal hidden relative -top-36 items-center justify-center">
+                <div class="modal-content bg-white p-4 sm:p-6 rounded shadow-lg w-3/4 md:w-2/3 lg:w-1/2 border">
+                    <span id="closeModal1" class="modal-close cursor-pointer md:p-4 text-xl md:text-2xl">&times;</span>
+                    <h2 class="font-bold md:text-lg mb-4 text-center">Penjadwalan Konselor</h2>
+
+                    <!-- Isi form modal -->
+                    <form class="flex flex-col mt-2 sm:mt-4">
+                        <!-- Tambahkan elemen form sesuai kebutuhan -->
+                        <div class="mb-2 sm:mb-4 flex items-center">
+                            <label for="namaKonseli" class="w-1/3 lg:w-1/4">Nama Konseli</label>
+                            <input type="text" id="namaKonseli" name="namaKonseli" class="p-2 border rounded w-2/3" value="Mahasiswa A" readonly>
+                        </div>
+                        <div class="mb-2 sm:mb-4 flex items-center">
+                            <label for="namaKonselor" class="w-1/3 lg:w-1/4">Nama Konselor</label>
+                            <input type="text" id="namaKonselor" name="namaKonselor" class="p-2 border rounded w-2/3" value="Konselor A" readonly>
+                        </div>
+                        <div class="mb-2 sm:mb-4 flex items-center">
+                            <label for="waktu" class="w-1/3 lg:w-1/4">Waktu Pertemuan</label>
+                            <input type="text" id="waktu" name="waktu" class="p-2 border rounded w-2/3" value="Waktu A" readonly>
+                        </div>
+                        <div class="mb-2 sm:mb-4 flex items-center">
+                            <label for="ruangan" class="w-1/3 lg:w-1/4">Ruangan Pertemuan</label>
+                            <select id="ruangan" name="ruangan" class="p-2 border rounded w-2/3">
+                                <option disabled selected>Pilih Ruangan</option>
+                                <option value="X">Ruang X</option>
+                                <option value="Y">Ruang Y</option>
+                                <option value="Z">Ruang Z</option>
+                            </select>
+                        </div>   
+
+                        <button type="submit" class="text-white font-bold py-1 sm:py-2 px-2 sm:px-4 w-1/3 sm:w-1/4 rounded mx-auto" style="background-color: #62ff7b;">
+                            Setujui
+                        </button>
+                    </form>
+                </div>
+            </div>
+
+            <!-- Modal Konseli -->
+            <div id="myModal2" class="modal hidden relative -top-36 items-center justify-center">
+                <div class="modal-content bg-white p-4 sm:p-6 rounded shadow-lg mx-auto w-3/4 md:w-2/3 lg:w-1/2 border">
+                    <span id="closeModal2" class="modal-close cursor-pointer md:p-4 text-xl md:text-2xl">&times;</span>
+                    <h2 class="font-bold md:text-lg mb-4 text-center">Penjadwalan Konseli</h2>
+
+                    <!-- Isi form modal -->
+                    <form class="flex flex-col mt-4">
+                        <!-- Tambahkan elemen form sesuai kebutuhan -->
+                        <div class="mb-4 flex items-center">
+                            <label for="namaKonseli" class="w-1/3">Nama Konseli</label>
+                            <input type="text" id="namaKonseli" name="namaKonseli" class="p-2 border rounded w-2/3" value="Mahasiswa B" readonly>
+                        </div>
+                        <div class="mb-4 flex items-center">
+                            <label for="namaKonselor" class="w-1/3">Nama Konselor</label>
+                            <select id="namaKonselor" name="namaKonselor" class="p-2 border rounded w-2/3">
+                                <option disabled selected>Pilih Konselor</option>
+                                <option value="A">Konselor A</option>
+                                <option value="B">Konselor B</option>
+                                <option value="C">Konselor C</option>
+                            </select>
+                        </div> 
+                        <div class="mb-4 flex items-center">
+                            <label for="waktu" class="w-1/3">Waktu Pertemuan</label>
+                            <select id="waktu" name="waktu" class="p-2 border rounded w-2/3">
+                                <option disabled selected>Waktu yang Diajukan</option>
+                                <option value="A">Waktu A</option>
+                                <option value="B">Waktu B</option>
+                                <option value="C">Waktu C</option>
+                            </select>
+                        </div>   
+                        <div class="mb-4 flex items-center">
+                            <label for="ruangan" class="w-1/3">Ruangan Pertemuan</label>
+                            <select id="ruangan" name="ruangan" class="p-2 border rounded w-2/3">
+                                <option disabled selected>Pilih Ruangan</option>
+                                <option value="X">Ruang X</option>
+                                <option value="Y">Ruang Y</option>
+                                <option value="Z">Ruang Z</option>
+                            </select>
+                        </div>   
+
+                        <div class="mb-4 flex items-center">
+                            <button type="submit" class="text-white font-bold py-2 px-4 rounded mx-auto" style="background-color: #ff7e62;">
+                            Ajukan Ulang
+                            </button>
+
+                            <!-- Additional button (Ajukan Ulang) -->
+                            <button type="button" class="text-white font-bold py-2 px-4 ml-4 rounded mx-auto" style="background-color: #62ff7b;">
+                            Setujui
+                            </button>
+                        </div>
+                    </form>
+
+                </div>
+            </div>         
         </main>
     </div>
 
-    <!-- Modal Konselor -->
-    <div id="myModal1" class="modal hidden fixed inset-0 bg-gray-950 flex items-center justify-center">
-        <div class="modal-content bg-white p-6 rounded shadow-lg mx-auto w-1/2 relative" style="left: 115px;">
-            <span id="closeModal1" class="modal-close cursor-pointer absolute top-2 right-2 p-4 text-2xl" style="right: 0; top: -10px;">&times;</span>
-            <h2 class="text-2xl font-bold mb-4 text-center">Penjadwalan Konselor</h2>
 
-            <!-- Isi form modal -->
-            <form class="flex flex-col mt-4">
-                <!-- Tambahkan elemen form sesuai kebutuhan -->
-                <div class="mb-4 flex items-center">
-                    <label for="namaKonseli" class="w-1/3">Nama Konseli</label>
-                    <input type="text" id="namaKonseli" name="namaKonseli" class="p-2 border rounded w-2/3" value="Mahasiswa A" readonly>
-                </div>
-                <div class="mb-4 flex items-center">
-                    <label for="namaKonselor" class="w-1/3">Nama Konselor</label>
-                    <input type="text" id="namaKonselor" name="namaKonselor" class="p-2 border rounded w-2/3" value="Konselor A" readonly>
-                </div>
-                <div class="mb-4 flex items-center">
-                    <label for="waktu" class="w-1/3">Waktu Pertemuan</label>
-                    <input type="text" id="waktu" name="waktu" class="p-2 border rounded w-2/3" value="Waktu A" readonly>
-                </div>
-                <div class="mb-4 flex items-center">
-                    <label for="ruangan" class="w-1/3">Ruangan Pertemuan</label>
-                    <select id="ruangan" name="ruangan" class="p-2 border rounded w-2/3">
-                        <option disabled selected>Pilih Ruangan</option>
-                        <option value="X">Ruang X</option>
-                        <option value="Y">Ruang Y</option>
-                        <option value="Z">Ruang Z</option>
-                    </select>
-                </div>   
-
-                <button type="submit" class="text-white font-bold py-2 px-4 w-1/4 rounded mx-auto" style="background-color: #62ff7b;">
-                    Setujui
-                </button>
-            </form>
-        </div>
-    </div>
-
-    <!-- Modal Konseli -->
-    <div id="myModal2" class="modal hidden fixed inset-0 bg-gray-950 flex items-center justify-center">
-        <div class="modal-content bg-white p-6 rounded shadow-lg mx-auto w-1/2 relative" style="left: 115px;">
-            <span id="closeModal2" class="modal-close cursor-pointer absolute top-2 right-2 p-4 text-2xl" style="right: 0; top: -10px;">&times;</span>
-            <h2 class="text-2xl font-bold mb-4 text-center">Penjadwalan Konseli</h2>
-
-            <!-- Isi form modal -->
-            <form class="flex flex-col mt-4">
-                <!-- Tambahkan elemen form sesuai kebutuhan -->
-                <div class="mb-4 flex items-center">
-                    <label for="namaKonseli" class="w-1/3">Nama Konseli</label>
-                    <input type="text" id="namaKonseli" name="namaKonseli" class="p-2 border rounded w-2/3" value="Mahasiswa B" readonly>
-                </div>
-                <div class="mb-4 flex items-center">
-                    <label for="namaKonselor" class="w-1/3">Nama Konselor</label>
-                    <select id="namaKonselor" name="namaKonselor" class="p-2 border rounded w-2/3">
-                        <option disabled selected>Pilih Konselor</option>
-                        <option value="A">Konselor A</option>
-                        <option value="B">Konselor B</option>
-                        <option value="C">Konselor C</option>
-                    </select>
-                </div> 
-                <div class="mb-4 flex items-center">
-                    <label for="waktu" class="w-1/3">Waktu Pertemuan</label>
-                    <select id="waktu" name="waktu" class="p-2 border rounded w-2/3">
-                        <option disabled selected>Waktu yang Diajukan</option>
-                        <option value="A">Waktu A</option>
-                        <option value="B">Waktu B</option>
-                        <option value="C">Waktu C</option>
-                    </select>
-                </div>   
-                <div class="mb-4 flex items-center">
-                    <label for="ruangan" class="w-1/3">Ruangan Pertemuan</label>
-                    <select id="ruangan" name="ruangan" class="p-2 border rounded w-2/3">
-                        <option disabled selected>Pilih Ruangan</option>
-                        <option value="X">Ruang X</option>
-                        <option value="Y">Ruang Y</option>
-                        <option value="Z">Ruang Z</option>
-                    </select>
-                </div>   
-
-                <div class="mb-4 flex items-center">
-                    <button type="submit" class="text-white font-bold py-2 px-4 w-1/4 rounded mx-auto" style="background-color: #ff7e62;">
-                    Ajukan Ulang
-                    </button>
-
-                    <!-- Additional button (Ajukan Ulang) -->
-                    <button type="button" class="text-white font-bold py-2 px-4 w-1/4 ml-4 rounded mx-auto" style="background-color: #62ff7b;">
-                    Setujui
-                    </button>
-                </div>
-            </form>
-
-        </div>
-    </div>
 
     <script>
         // JavaScript untuk mengatur tampilan modal Konselor
+        const container = document.getElementById('container');
         const openModalBtn1 = document.getElementById('openModal1');
         const closeModalBtn1 = document.getElementById('closeModal1');
         const modal1 = document.getElementById('myModal1');
 
         openModalBtn1.addEventListener('click', () => {
-            modal1.classList.remove('hidden');
+            modal1.classList.replace('hidden', 'flex');
+            container.classList.add('blur-[2px]');
         });
 
         closeModalBtn1.addEventListener('click', () => {
-            modal1.classList.add('hidden');
+            modal1.classList.replace('flex','hidden');
+            container.classList.remove('blur-[2px]');
         });
 
         // JavaScript untuk mengatur tampilan modal Konseli
@@ -154,11 +154,13 @@
         const modal2 = document.getElementById('myModal2');
 
         openModalBtn2.addEventListener('click', () => {
-            modal2.classList.remove('hidden');
+            modal2.classList.replace('hidden', 'flex');
+            container.classList.add('blur-[2px]');
         });
 
         closeModalBtn2.addEventListener('click', () => {
-            modal2.classList.add('hidden');
+            modal2.classList.replace('flex','hidden');
+            container.classList.remove('blur-[2px]');
         });
     </script>
 @endsection
